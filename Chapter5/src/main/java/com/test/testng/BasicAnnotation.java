cd700b6f71b7ffcbbf26b95c383a146cbada6024
lvxueqing <2842726905@qq.com>
2023-02-27 15:04:49 +0800

    新增testng基础标签@Test代码

---
+++ Chapter5/src/main/java/com/test/testng/BasicAnnotation.java
@@ -0,0 +1,19 @@
+package com.test.testng;
+
+import org.testng.annotations.Test;
+
+/**
+ * testng基础标签
+ *
+ * @program：testFrameWithJava
+ * @author: lvxueqing
+ * @create: 2023/02/27 15:00
+ */
+public class BasicAnnotation {
+
+
+    @Test
+    public void testCase1(){
+        System.out.println("测试用例1");
+    }
+}
