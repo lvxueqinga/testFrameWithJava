a98cc779dd344ef21ce640d2c99f1ee8b18c5ec8
lvxueqing <2842726905@qq.com>
2023-03-04 13:50:42 +0800

    springboot 携带cookies信息的get请求

--- Chapter10/src/main/java/com/course/server/MyGetMethod.java
+++ Chapter10/src/main/java/com/course/server/MyGetMethod.java
@@ -7,6 +7,7 @@ import org.springframework.web.bind.annotation.RestController;
 import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import java.util.Objects;
 
 /**
  * get方法
@@ -19,6 +20,9 @@ import javax.servlet.http.HttpServletResponse;
 @RestController
 public class MyGetMethod {
 
+    /**
+     * 返回cookie信息的get接口
+     */
     @RequestMapping(value = "/getcookies",method = RequestMethod.GET)
     public String getCookies(HttpServletResponse response){
 
@@ -29,6 +33,28 @@ public class MyGetMethod {
         response.addCookie(cookie);
 
         return  "恭喜获得cookies信息成功";
+    }
+
+    /**
+     * 要求携带cookie访问的get请求
+     */
+
+    @RequestMapping(value = "/get/with/cookies",method = RequestMethod.GET)
+    public String getWithCookies(HttpServletRequest request){
+
+        Cookie[] cookies = request.getCookies();
+
+        if (Objects.isNull(cookies)){
+            return "访问失败 无cookies";
+        }
+
+        for (Cookie c:cookies){
+            if (c.getName().equals("login") && c.getValue().equals("true")){
+                return "成功 这是一个需要携带cookies信息才能访问的get请求";
+            }
+
+        }
+        return "访问失败 无cookies";
     }
 
 }
