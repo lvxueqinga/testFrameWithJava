6421c5a8f801dcefa716eec69f694a5961ee71f9
lvxueqing <2842726905@qq.com>
2023-03-14 16:58:13 +0800

    chapter12 是完整的测试框架 部分测试用例未调试， chapter13做为服务端接口， 两个模块配合

--- Chapter12/src/main/java/com/course/cases/LoginTest.java
+++ Chapter12/src/main/java/com/course/cases/LoginTest.java
@@ -5,8 +5,13 @@ import com.course.model.InterfaceName;
 import com.course.model.LoginCase;
 import com.course.utils.ConfigFile;
 import com.course.utils.DatabaseUtil;
+import org.apache.http.HttpResponse;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.entity.StringEntity;
 import org.apache.http.impl.client.DefaultHttpClient;
+import org.apache.http.util.EntityUtils;
 import org.apache.ibatis.session.SqlSession;
+import org.json.JSONObject;
 import org.testng.Assert;
 import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
@@ -15,6 +20,9 @@ import java.io.IOException;
 
 /**
  * 登陆
+ *
+ * 从loginCase表里取数据， 用来测试是否登陆成功，
+ * 如果与loginuser表里账号密码一致，则成功， 不一致 则登陆失败
  *
  * @program：testFrameWithJava
  * @author: lvxueqing
@@ -39,11 +47,12 @@ public class LoginTest {
         LoginCase loginCase = session.selectOne("loginCase",1);
         System.out.println(loginCase.toString());
         System.out.println(TestConfig.loginUrl);
+        System.out.println("------------loginCase.getExpected()-----------"+loginCase.getExpected());
 
-//        //下边的代码为写完接口的测试代码
-//        String result = getResult(loginCase);
-//        //处理结果，就是判断返回结果是否符合预期
-//        Assert.assertEquals(loginCase.getExpected(),result);
+        //下边的代码为写完接口的测试代码
+        String result = getResult(loginCase);
+        //处理结果，就是判断返回结果是否符合预期
+        Assert.assertEquals(loginCase.getExpected(),result);
 
 
     }
@@ -54,14 +63,37 @@ public class LoginTest {
         LoginCase loginCase = session.selectOne("loginCase",2);
         System.out.println(loginCase.toString());
         System.out.println(TestConfig.loginUrl);
+        System.out.println("------------loginCase.getExpected()-----------"+loginCase.getExpected());
 
 
 
-//        //下边的代码为写完接口的测试代码
-//        String result = getResult(loginCase);
-//        //处理结果，就是判断返回结果是否符合预期
-//        Assert.assertEquals(loginCase.getExpected(),result);
+        //下边的代码为写完接口的测试代码
+        String result = getResult(loginCase);
+        //处理结果，就是判断返回结果是否符合预期
+        Assert.assertEquals(loginCase.getExpected(),result);
 
+    }
+
+    private String getResult(LoginCase loginCase) throws IOException {
+        //下边的代码为写完接口的测试代码
+        HttpPost post = new HttpPost(TestConfig.loginUrl);
+        JSONObject param = new JSONObject();
+        param.put("username",loginCase.getUsername());
+        param.put("password",loginCase.getPassword());
+        //设置请求头信息 设置header
+        post.setHeader("content-type","application/json");
+        //将参数信息添加到方法中
+        StringEntity entity = new StringEntity(param.toString(),"utf-8");
+        post.setEntity(entity);
+        //声明一个对象来进行响应结果的存储
+        String result;
+        //执行post方法
+        HttpResponse response = TestConfig.defaultHttpClient.execute(post);
+        //获取响应结果
+        result = EntityUtils.toString(response.getEntity(),"utf-8");
+        System.out.println("----------getresult----------"+result);
+        TestConfig.store = TestConfig.defaultHttpClient.getCookieStore();
+        return result;
     }
 
 }
